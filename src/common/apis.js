ab1158b000ff215dda71f8383f66531a40076d13
Sayali <sayalivetal@bootesnull.com>
2022-10-07 12:53:35 +0530

    listing page bugs fix

--- src/common/apis.js
+++ src/common/apis.js
@@ -2,8 +2,8 @@
 // export const API = "http://kol.bootesnull.com/api";
 // export const imageUrl = "http://kol.bootesnull.com";
 
-export const API = 'https://b5a7-203-145-168-10.in.ngrok.io/api';
-export const imageUrl = 'https://b5a7-203-145-168-10.in.ngrok.io';
+export const API = 'https://d99f-203-145-168-10.in.ngrok.io/api';
+export const imageUrl = 'https://d99f-203-145-168-10.in.ngrok.io';
 
 
 
