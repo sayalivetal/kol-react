448bd00e5db5c7c885f1616b3eb3b2c2e49ecd8c
navjyotBootes <navjyot.engotra@bootesnull.com>
2022-11-01 17:27:30 +0530

    end user validation

--- src/common/apis.js
+++ src/common/apis.js
@@ -2,8 +2,8 @@
 // export const API = "http://kol.bootesnull.com/api";
 // export const imageUrl = "http://kol.bootesnull.com";
 
-export const API = 'https://0d8f-203-145-168-10.in.ngrok.io/api';
-export const imageUrl = 'https://0d8f-203-145-168-10.in.ngrok.io';
+export const API = 'https://2bbb-203-145-168-10.in.ngrok.io/api';
+export const imageUrl = 'https://2bbb-203-145-168-10.in.ngrok.io';
 
 
 
